022ee8bd5bc4f3878ad03f954cf5e78d09c1c604
Oksana0020 <oksana.fursa@students.ittralee.ie>
2024-10-07 21:21:03 +0100

    Place registration data into controller

--- app_server/controllers/registrations.js
+++ app_server/controllers/registrations.js
@@ -1,15 +1,47 @@
-/* GET 'login' page */
-const login = function(req, res) {
-  res.render('login', { title: 'Login' });
+/* GET 'registration' page */
+const register = function(req, res) {
+  const info = { 
+    title: 'Registration',
+    form: {
+      firstName: {
+        placeholder: 'Enter your First Name'
+      },
+      lastName: {
+        placeholder: 'Enter your Last Name'
+      },
+      username: {
+        placeholder: 'Enter your Username'
+      },
+      email: {
+        placeholder: 'Enter your E-mail'
+      },
+      password: {
+        placeholder: 'Enter your Password'
+      },
+      confirm: {
+        placeholder: 'Repeat your Password'
+      },
+      favouriteFlower: {
+        placeholder: 'Enter your Favourite flower'
+      }
+    }
+  };
+
+  console.log('Info sent to registration.pug:', info); 
+  res.render('registration', { info }); 
 };
 
-/* GET 'Register' page */
-const register = function(req, res) {
-  res.render('registration', { title: 'Register' });
+/* GET 'login' page */
+const login = function(req, res) {
+  const info = { 
+    title: 'Login'
+  };
+  
+  console.log('Info sent to login.pug:', info); 
+  res.render('login', { info }); 
 };
 
 module.exports = {
-  login,
-  register
+  register,
+  login
 };
-
