18aa053029229c35dac9dc8c280285ad6aa397da
Oksana0020 <oksana.fursa@students.ittralee.ie>
2024-10-07 20:27:46 +0100

    Try to place Dahlia data into controller

--- app_server/controllers/data.js
+++ app_server/controllers/data.js
@@ -1,10 +1,24 @@
-/* GET 'data' page */
 const data = function(req, res) {
-  console.log('Data page requested');
-  res.render('data', { title: 'Data' });
+  res.render('data', {
+    title: 'Data',   
+    flower: {       
+      name: 'Dahlia',
+      picture: '../images/1.jpg',   
+      rating: 3,
+      description: 'Dahlias are perennial plants with tuberous roots',
+      careGuide: {
+        level: 'beginner',
+        temperature: '29°C',
+        light: 'full sun',
+        water: 'every week',
+        fertilize: 'high in potassium and phosphorus',
+        bloomTime: 'summer, autumn',
+      },
+      votes: 53,
+    }
+  });
 };
 
 module.exports = {
   data
 };
-
