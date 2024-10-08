0479e5b9bddccbffffac00f486a8110da719057c
Oksana0020 <oksana.fursa@students.ittralee.ie>
2024-10-08 22:41:13 +0100

    Fixed errors in data controller

--- app_server/controllers/data.js
+++ app_server/controllers/data.js
@@ -1,20 +1,20 @@
 const data = function(req, res) {
   res.render('data', {
-    title: 'Data',   
-    flower: {       
-      name: 'Dahlia',
-      picture: '../images/1.jpg',   
-      rating: 3,
-      description: 'Dahlias are perennial plants with tuberous roots',
+    title: 'Data',    
+    product: {
+      name: 'Dahlia',   
+      picture: '/images/1.jpg',   
+      rating: 3,    
+      description: 'Dahlias are perennial plants with tuberous roots, though they are grown as annuals in some regions with cold winters. While some have herbaceous stems, others have stems which lignify in the absence of secondary tissue and resprout following winter dormancy, allowing further seasons of growth.',
       careGuide: {
-        level: 'beginner',
-        temperature: '29°C',
-        light: 'full sun',
-        water: 'every week',
-        fertilize: 'high in potassium and phosphorus',
-        bloomTime: 'summer, autumn',
+        level: 'beginner',    
+        temperature: '29°C',   
+        light: 'full sun',   
+        water: 'every week',  
+        fertilize: 'high in potassium and phosphorus',   
+        bloomTime: 'summer, autumn'  
       },
-      votes: 53,
+      votes: 53    
     }
   });
 };
