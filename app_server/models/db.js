c4a45d88e11994c9bf05a334095745c84feadf28
Oksana0020 <oksana.fursa@students.ittralee.ie>
2024-10-15 23:15:46 +0100

    added a database connection and data models

---
+++ app_server/models/db.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const dbURI =
+    "mongodb+srv://user1:mtu12345@cluster0.ykc32.mongodb.net/PrettyPetals?retryWrites=true&w=majority&appName=Cluster0";
+
+try {
+    mongoose.connect(dbURI) 
+        .then(() => {
+            console.log("Mongoose is connected");
+        })
+        .catch(err => {
+            console.log(err);
+        });
+} catch (e) {
+    console.log("Could not connect", e);
+}
+
+require('./locations'); 
+
---
+++ app_server/models/locations.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+
+// CareGuide Schema
+const careGuideSchema = new mongoose.Schema({
+    level: {
+        type: String,
+        required: true
+    },
+    temperature: {
+        type: String,
+        required: true
+    },
+    light: {
+        type: String,
+        required: true
+    },
+    water: {
+        type: String,
+        required: true
+    },
+    fertilize: {
+        type: String,
+        required: true
+    },
+    bloomTime: {
+        type: String,
+        required: true
+    }
+});
+
+// Flower Schema
+const flowerSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    picture: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
+        default: 0,
+        min: 0,
+        max: 5
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    careGuide: careGuideSchema, 
+    votes: {
+        type: Number,
+        default: 0
+    }
+});
+
+// Create Mongoose model for flowers
+mongoose.model('Flower', flowerSchema, 'flowers');
+
+//look in db for 3rd parameter
