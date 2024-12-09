e1606472187d16b9455d9c6365a9288bc039df23
Oksana0020 <oksana.fursa@students.ittralee.ie>
2024-12-09 07:55:28 +0000

    Updated routing

--- PrettyPetals-public/src/app/app.module.ts
+++ PrettyPetals-public/src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { HttpHeaders, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'; 
 
 import { HomeListComponent } from './home-list/home-list.component';
 
+// Define the application routes
+const routes: Routes = [
+  { path: '', component: HomeListComponent }, 
+  { path: 'data', component: HomeListComponent },
+];
+
 @NgModule({
   declarations: [
-    HomeListComponent,
+    HomeListComponent, 
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes) 
   ],
   providers: [],
-  bootstrap: [HomeListComponent]
+  bootstrap: [HomeListComponent] 
 })
 export class AppModule { }
