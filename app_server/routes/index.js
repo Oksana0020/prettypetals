215a4fc4e32ecc6f9320f21b52e4bf4b018124b9
Oksana0020 <oksana.fursa@students.ittralee.ie>
2024-12-09 08:46:38 +0000

    Build with adjusted path

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,20 +1,24 @@
 const express = require('express');
+const path = require('path'); 
 const router = express.Router();
 const authController = require('../controllers/auth');
 const registrationController = require('../controllers/registrations');
-const ctrlData = require('../controllers/data');
 const ctrlFlowers = require('../../app_api/controllers/flowers');
 
-// Login Pages (Auth Controller)
+// Login Pages 
 router.get('/login', authController.login);
 router.post('/login', authController.processLogin);
 
-// Registration Pages (Registration Controller)
+// Registration Pages 
 router.get('/registration', registrationController.register);
 router.post('/registration', registrationController.processRegistration);
 
-// Data Pages
-router.get('/data', ctrlData.getFlowersData);
+// API Endpoint for Flowers
 router.get('/flowers', ctrlFlowers.flowersList);
+
+//Angular application for all other undefined routes
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../dist/pretty-petals-public/browser/index.html'));
+});
 
 module.exports = router;
